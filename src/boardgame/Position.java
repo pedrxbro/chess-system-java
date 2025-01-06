1446d080f67cb517a6b4bd16fa4b842ac3841b45
pedrxbro <pedrohdpaula@edu.univali.br>
2025-01-06 20:54:04 -0300

    Colocando peças no tabuleiro

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -4,9 +4,9 @@ public class Position {
     private int row;
     private int column;
 
-    public Position(int column, int row) {
-        this.column = column;
+    public Position(int row, int column) {
         this.row = row;
+        this.column = column;
     }
 
     public int getColumn() {
