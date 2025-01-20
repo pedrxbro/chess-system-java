8c2d1ef557037ba2ca4bd51b3b825db2650c1991
pedrxbro <pedrohdpaula@edu.univali.br>
2025-01-20 11:41:11 -0300

    Contando a movimentação das peças

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -22,6 +23,17 @@ public abstract class ChessPiece extends Piece {
     protected boolean isThereOpponentPiece(Position position){
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+    protected void increaseMoveCount(){
+        moveCount++;
+    }
+
+    protected void decreaseMoveCount(){
+        moveCount--;
     }
 
 }
