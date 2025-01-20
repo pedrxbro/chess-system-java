6e614c3b81a3460033d16c0a3c8174c118d5931d
pedrxbro <pedrohdpaula@edu.univali.br>
2025-01-20 18:40:36 -0300

    Peão

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,70 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece {
+//NAO ESTÁ FUNCIONANDO A JOGADA DE DUAS CASAS DO PEAO: VERIFICAR
+    public Pawn(Board board, Color color) { super(board, color);}
+    @Override
+    public String toString(){
+        return "P";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        if(getColor() == Color.BRANCO){
+            //Uma casa para frente.
+            p.setValues(position.getRow() - 1, position.getColumn());
+            if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //Duas casas para frente, na primeira jogada.
+            p.setValues(position.getRow() - 2, position.getColumn());
+            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //Diagonal esq, para eliminar a peça do oponente
+            p.setValues(position.getRow() - 1, position.getColumn() - 1);
+            if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //Diagonal dir, para eliminar a peça do oponente
+            p.setValues(position.getRow() - 1, position.getColumn() + 1);
+            if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+        else{
+            //Uma casa atrás.
+            p.setValues(position.getRow() + 1, position.getColumn());
+            if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //Duas casas atrás, na primeira jogada.
+            p.setValues(position.getRow() + 2, position.getColumn());
+            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //Diagonal esq, para eliminar a peça do oponente
+            p.setValues(position.getRow() + 1, position.getColumn() - 1);
+            if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            //Diagonal dir, para eliminar a peça do oponente
+            p.setValues(position.getRow() + 1, position.getColumn() + 1);
+            if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+        return mat;
+    }
+}
